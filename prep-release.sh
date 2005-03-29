f45057f6b326493f5783141492cc051bd4b93e37
Paul Pelzl <pelzlpj@eecs.umich.edu>
2005-03-29 05:16:20 +0000

    pulled in build script from Orpie

---
+++ prep-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 'prep-release' script
+# Make all the last-minute changes to prepare the sources for packaging
+# in a release tarball
+
+echo "Loading source dependencies..."
+tla build-config dist.arch
+echo "Removing {arch} directories..."
+rm -rf "{arch}"
+rm -rf "curses/{arch}"
+echo "Generating ./configure ..."
+autoconf && rm -rf autom4te.cache
+#echo "Creating documentation..."
+#cd doc && make &> /dev/null
+#echo "Done."
+
+
+
+
+# arch-tag: DO_NOT_CHANGE_f08da183-17c9-42c8-904b-7d6bab8dfe50 
