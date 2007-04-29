f258e0c1e0a681b447b6ebea4187b6fe61f5d455
Paul Pelzl <pelzlpj@eecs.umich.edu>
2007-04-28 19:06:52 -0500

    update prep-release script to reflect bzr setup

--- prep-release.sh
+++ prep-release.sh
@@ -2,20 +2,21 @@
 # 'prep-release' script
 # Make all the last-minute changes to prepare the sources for packaging
 # in a release tarball
+#
+# Usage: prep-release.sh DESTDIR
+#
 
-echo "Loading source dependencies..."
-baz build-config dist.arch
-echo "Preprocessing curses bindings..."
-make -C curses
-echo "Removing {arch} directories..."
-rm -rf "{arch}"
-rm -rf "curses/{arch}"
+echo "Exporting revision..."
+bzr export $1
+echo "Exporting dependencies..."
+bzr export $1/curses $HOME/src/bzr-repo/libcurses-ocaml-dev
+
+cd $1
 echo "Generating ./configure ..."
 autoconf && rm -rf autom4te.cache
 echo "Creating documentation..."
 cd doc && make &> /dev/null
 echo "Done."
-
 
 
 
